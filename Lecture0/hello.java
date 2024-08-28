5a204769674f90d0b5a6a9204453d66c5102c83f
subodh rana <subodh.2003.rana@gmail.com>
2024-08-28 14:44:54 +0530

    message

---
+++ Lecture0/hello.java
@@ -0,0 +1,10 @@
+package Lecture0;
+
+public class hello{
+public static void main(String[]arg){
+  System.out.println("  hello World");
+  int a  = 10;
+  int b = 10;
+  System.out.println(b+a);
+}
+}
